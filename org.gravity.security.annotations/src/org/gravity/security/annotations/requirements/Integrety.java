73dd4b9b3552c5bc528e9eb17ae28c269c614254
Sven Peldszus <speldszus@uni-koblenz.de>
2017-01-10 08:22:20 +0100

    First try for call path search and security annotations

---
+++ org.gravity.security.annotations/src/org/gravity/security/annotations/requirements/Integrety.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.gravity.security.annotations.requirements;
+
+import static java.lang.annotation.ElementType.FIELD;
+
+import java.lang.annotation.Target;
+
+@Target(FIELD)
+/**
+ * @author speldszus
+ *
+ */
+public @interface Integrety {
+
+}
