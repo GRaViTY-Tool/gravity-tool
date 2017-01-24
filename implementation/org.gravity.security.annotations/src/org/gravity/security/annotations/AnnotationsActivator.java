62d32f99ec9d4e5927300d99060c3ac441344826
Sven Peldszus <speldszus@uni-koblenz.de>
2017-01-24 15:09:48 +0100

    Fixed bugs from merging

--- implementation/org.gravity.security.annotations/src/org/gravity/security/annotations/AnnotationsActivator.java
+++
@@ -1,9 +0,0 @@
-package org.gravity.security.annotations;
-
-import org.eclipse.core.runtime.Plugin;
-
-public class AnnotationsActivator extends Plugin {
-	
-	public static final String PLUGIN_ID = "org.gravity.security.annotations";
-
-}
