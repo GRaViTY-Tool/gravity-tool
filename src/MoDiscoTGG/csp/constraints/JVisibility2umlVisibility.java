4ee000abbcd147c2f478d7a101727dfa337393ab
Fabian Bauer <fabian_marco.bauer@stud.tu-darmstadt.de>
2017-06-13 15:19:08 +0200

    fix JVisibility2umlVisibility for package visibility

--- src/MoDiscoTGG/csp/constraints/JVisibility2umlVisibility.java
+++ src/MoDiscoTGG/csp/constraints/JVisibility2umlVisibility.java
@@ -1,29 +1,38 @@
 package MoDiscoTGG.csp.constraints;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.moflon.tgg.csp.constraints.*;
 import org.moflon.tgg.language.csp.Variable;
 import org.moflon.tgg.language.csp.impl.TGGConstraintImpl;
 
 public class JVisibility2umlVisibility extends TGGConstraintImpl {
+	private static final Map<org.eclipse.gmt.modisco.java.VisibilityKind, org.eclipse.uml2.uml.VisibilityKind> jToU = new HashMap<>(4);
+	private static final Map<org.eclipse.uml2.uml.VisibilityKind, org.eclipse.gmt.modisco.java.VisibilityKind> uToJ = new HashMap<>(4);
+	
+	static {
+		jToU.put(org.eclipse.gmt.modisco.java.VisibilityKind.PRIVATE, org.eclipse.uml2.uml.VisibilityKind.PRIVATE_LITERAL);
+		jToU.put(org.eclipse.gmt.modisco.java.VisibilityKind.PROTECTED, org.eclipse.uml2.uml.VisibilityKind.PROTECTED_LITERAL);
+		jToU.put(org.eclipse.gmt.modisco.java.VisibilityKind.NONE, org.eclipse.uml2.uml.VisibilityKind.PACKAGE_LITERAL);
+		jToU.put(org.eclipse.gmt.modisco.java.VisibilityKind.PUBLIC, org.eclipse.uml2.uml.VisibilityKind.PUBLIC_LITERAL);
+	}
+	
 	public void solve(Variable var_0, Variable var_1){
     	String bindingStates = getBindingStates(var_0, var_1);
     	
-    	if (var_0.isBound()) {
-    		System.out.println("var_0 "+((org.eclipse.gmt.modisco.java.VisibilityKind)var_0.getValue()).getName()+"->"+org.eclipse.uml2.uml.VisibilityKind.getByName(((org.eclipse.gmt.modisco.java.VisibilityKind)var_0.getValue()).getName()));
-    	}
-    	if (var_1.isBound()) {
-    		System.out.println("var_1 "+((org.eclipse.uml2.uml.VisibilityKind)var_0.getValue()).getName()+"->"+org.eclipse.gmt.modisco.java.VisibilityKind.getByName(((org.eclipse.uml2.uml.VisibilityKind)var_1.getValue()).getName()));
-    	}
-    	
     	switch(bindingStates){
     	case "BB":
-    		setSatisfied(((org.eclipse.gmt.modisco.java.VisibilityKind)var_0.getValue()).getName().equals(((org.eclipse.uml2.uml.VisibilityKind)var_1.getValue()).getName()));
+    		setSatisfied(jToU.get(var_0.getValue())==var_1.getValue());
+    		break;
     	case "BF":
-    		var_1.bindToValue(org.eclipse.uml2.uml.VisibilityKind.getByName(((org.eclipse.gmt.modisco.java.VisibilityKind)var_0.getValue()).getName()));
+    		var_1.bindToValue(jToU.get(var_0.getValue()));
     		setSatisfied(true);
+    		break;
     	case "FB":
-    		var_0.bindToValue(org.eclipse.gmt.modisco.java.VisibilityKind.getByName(((org.eclipse.uml2.uml.VisibilityKind)var_1.getValue()).getName()));
+    		var_0.bindToValue(uToJ.get(var_1.getValue()));
     		setSatisfied(true);
+    		break;
     	default: 
     		throw new UnsupportedOperationException("This case in the constraint has not been implemented yet: " + bindingStates);
     	}
