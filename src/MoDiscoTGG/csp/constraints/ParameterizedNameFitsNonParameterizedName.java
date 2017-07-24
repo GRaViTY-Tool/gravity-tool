31c145fb72073d5b7ee7336b2a6f8bfc00421041
Fabian Bauer <fabian_marco.bauer@stud.tu-darmstadt.de>
2017-07-24 10:17:17 +0200

    fix attributeConditions for ParameterizedType2...

--- src/MoDiscoTGG/csp/constraints/ParameterizedNameFitsNonParameterizedName.java
+++ src/MoDiscoTGG/csp/constraints/ParameterizedNameFitsNonParameterizedName.java
@@ -17,38 +17,13 @@ public class ParameterizedNameFitsNonParameterizedName extends TGGConstraintImpl
 	 * @param var_1
 	 *            the name of the nonParameterizedType
 	 */
-	public void solve(Variable var_0, Variable var_1, Variable var_2, Variable var_3) {
-		String bindingStates = getBindingStates(var_0, var_1, var_2, var_3);
-		String nonParameterizedName1 = (String) var_2.getValue();
-		String nonParameterizedName2 = (String) var_3.getValue();
-		if (!nonParameterizedName1.equals(nonParameterizedName2)) {
-			setSatisfied(false);
-			return;
-		}
-		String parameterizedName1, parameterizedName2;
+	public void solve(Variable var_0, Variable var_1) {
+		String bindingStates = getBindingStates(var_0, var_1);
 		switch (bindingStates) {
-		case "BBBB":
-			parameterizedName1 = (String) var_0.getValue();
-			parameterizedName2 = (String) var_1.getValue();
-			if (!parameterizedName1.equals(parameterizedName2)) {
-				setSatisfied(false);
-				return;
-			}
-			setSatisfied(parameterizedName1.matches("(.*\\.)?" + nonParameterizedName1 + "<.*"));
-			return;
-		case "FBBB":
-			parameterizedName2 = (String) var_1.getValue();
-			setSatisfied(parameterizedName2.matches("(.*\\.)?" + nonParameterizedName2 + "<.*"));
-			if (satisfied) {
-				var_0.bindToValue(parameterizedName2);
-			}
-			return;
-		case "BFBB":
-			parameterizedName1 = (String) var_0.getValue();
-			setSatisfied(parameterizedName1.matches("(.*\\.)?" + nonParameterizedName1 + "<.*"));
-			if (satisfied) {
-				var_1.bindToValue(parameterizedName1);
-			}
+		case "BB":
+			String parameterizedName = (String) var_0.getValue();
+			String nonParameterizedName = (String) var_1.getValue();
+			setSatisfied(parameterizedName.matches("(.*\\.)?" + nonParameterizedName + "<.*"));
 			return;
 		default:
 			throw new UnsupportedOperationException(
