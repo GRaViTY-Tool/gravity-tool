dbadc119f64a61d5a1d4e0f64c82d9acb09d0e60
Sven Peldszus <speldszus@uni-koblenz.de>
2018-10-11 14:04:23 +0200

    Removed blocking Bug and improved overall quality

--- implementation/org.gravity.metrics.sourcemeter/src/org/gravity/metrics/sourcemeter/MetricCalculator.java
+++ implementation/org.gravity.metrics.sourcemeter/src/org/gravity/metrics/sourcemeter/MetricCalculator.java
@@ -2,70 +2,77 @@ package org.gravity.metrics.sourcemeter;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Hashtable;
+import java.util.HashMap;
 import java.util.Map.Entry;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 import javax.swing.JOptionPane;
 
+/**
+ * This class provides the functionality to execute SourceMeter and get values
+ * of specific metrics
+ * 
+ * @see https://www.sourcemeter.com/
+ * 
+ * @author speldszus
+ *
+ */
 public class MetricCalculator {
 
-	private Hashtable<String, String[]> results;
-	private Hashtable<String, String> unqualified;
+	private HashMap<String, String[]> results = new HashMap<>();
+	private HashMap<String, String> unqualified = new HashMap<>();
 
-	private static final Logger LOGGER = Logger.getLogger( MetricCalculator.class.getName() );
-	
-	public MetricCalculator() {
-		this.results = new Hashtable<>();
-		this.unqualified = new Hashtable<>();
-	}
+	private static final Logger LOGGER = Logger.getLogger(MetricCalculator.class.getName());
 
 	private static final String SRC_METER_FOLDER = "SrcMeter"; //$NON-NLS-1$
-	public final String env_variable_name = "SOURCE_METER_JAVA"; //$NON-NLS-1$
+	private static final String ENV_VARIABLE_NAME = "SOURCE_METER_JAVA"; //$NON-NLS-1$
 
-	public SourceMeterStatus calculateMetrics(File program_location, File out_location) {
-		File src_meter_folder = new File(out_location, SRC_METER_FOLDER);
-		clear(src_meter_folder);
-		String[] params = {"-projectName=" + SRC_METER_FOLDER, //$NON-NLS-1$
-				"-projectBaseDir=" + program_location.toString(), //$NON-NLS-1$
-				"-resultsDir=" + out_location.toString() }; //$NON-NLS-1$
-		
-		SourceMeterStatus status = run(this.env_variable_name, params);
-		if(!SourceMeterStatus.OK.equals(status)){
+	/**
+	 * This method executes sourcemater on a given location
+	 * 
+	 * @param programLocation The location of the source code on which sourcemeter
+	 *                        should be executed
+	 * @param outputLocation  The location to which sourcemeter should write the
+	 *                        results
+	 * @return
+	 */
+	public SourceMeterStatus calculateMetrics(File programLocation, File outputLocation) {
+		File sourcemeterOutputFolder = new File(outputLocation, SRC_METER_FOLDER);
+		clear(sourcemeterOutputFolder);
+		String[] params = { "-projectName=" + SRC_METER_FOLDER, //$NON-NLS-1$
+				"-projectBaseDir=" + programLocation.toString(), //$NON-NLS-1$
+				"-resultsDir=" + outputLocation.toString() }; //$NON-NLS-1$
+
+		SourceMeterStatus status = run(MetricCalculator.ENV_VARIABLE_NAME, params);
+		if (!SourceMeterStatus.OK.equals(status)) {
 			return status;
 		}
 
-		File[] java_folder = new File(src_meter_folder, "java").listFiles(); //$NON-NLS-1$
-		if (java_folder.length > 0) {
-			String[] files = {"SrcMeter-Class.csv", "SrcMeter-Enum.csv"} ;
-			for(String f : files){
-			File metrics = new File(java_folder[0], f); //$NON-NLS-1$
-			try {
-				BufferedReader file_reader = new BufferedReader(new FileReader(metrics));
-				String line = file_reader.readLine();
-				if (line == null) {
-					file_reader.close();
-					LOGGER.log(Level.ERROR, "Sourcemeter metric file is empty");
+		File[] sourcemeterJavaFolder = new File(sourcemeterOutputFolder, "java").listFiles(); //$NON-NLS-1$
+		if (sourcemeterJavaFolder.length > 0) {
+			String[] files = { "SrcMeter-Class.csv", "SrcMeter-Enum.csv" };
+			for (String f : files) {
+				File metrics = new File(sourcemeterJavaFolder[0], f); // $NON-NLS-1$
+				try (BufferedReader fileReader = new BufferedReader(new FileReader(metrics))) {
+					String line = fileReader.readLine();
+					if (line == null) {
+						fileReader.close();
+						LOGGER.log(Level.ERROR, "Sourcemeter metric file is empty");
+						return SourceMeterStatus.ERROR;
+					}
+					while ((line = fileReader.readLine()) != null) {
+						String[] values = line.substring(1, line.length() - 1).split("\",\""); //$NON-NLS-1$
+						this.results.put(values[2], values);
+						this.unqualified.put(values[1], values[2]);
+					}
+				} catch (IOException e) {
+					LOGGER.log(Level.ERROR, e.getMessage(), e);
 					return SourceMeterStatus.ERROR;
 				}
-				while ((line = file_reader.readLine()) != null) {
-					String[] values = line.substring(1, line.length() - 1).split("\",\""); //$NON-NLS-1$
-					this.results.put(values[2], values);
-					this.unqualified.put(values[1], values[2]);
-				}
-				file_reader.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-				return SourceMeterStatus.ERROR;
-			} catch (IOException e) {
-				e.printStackTrace();
-				return SourceMeterStatus.ERROR;
-			}
 			}
 		} else {
 			return SourceMeterStatus.ERROR;
@@ -74,113 +81,148 @@ public class MetricCalculator {
 		return SourceMeterStatus.OK;
 	}
 
-	public SourceMeterStatus run(String exec, String[] params) {
-		String src_meter = System.getenv(exec);
-		if (src_meter == null) {
+	private SourceMeterStatus run(String exec, String[] params) {
+		String sourcemmeter = System.getenv(exec);
+		if (sourcemmeter == null) {
 			JOptionPane.showMessageDialog(null,
 					"Please download sourcemeter from \"www.sourcemeter.com\" and set the environment variable \""
-							+ env_variable_name + "\" to the path of the \"SourceMeterJava\" file.");
+							+ ENV_VARIABLE_NAME + "\" to the path of the \"SourceMeterJava\" file.");
 			return SourceMeterStatus.NOT_INSTALLED;
 		}
-		if(!new File(src_meter).exists()){
+		if (!new File(sourcemmeter).exists()) {
 			LOGGER.log(Level.WARN, "Sourcemeter is not installed at the specified location!");
 			JOptionPane.showMessageDialog(null,
 					"Sourcemeter is not installed at the specified location! Set the environment variable \""
-							+ env_variable_name + "\" to the path of the \"SourceMeterJava\" file.");
+							+ ENV_VARIABLE_NAME + "\" to the path of the \"SourceMeterJava\" file.");
 			return SourceMeterStatus.NOT_INSTALLED;
 		}
 
-		String[] cmd = new String[params.length+1];
-		cmd[0] = src_meter;
+		String[] cmd = new String[params.length + 1];
+		cmd[0] = sourcemmeter;
 		System.arraycopy(params, 0, cmd, 1, params.length);
 		Runtime run = Runtime.getRuntime();
 		try {
 			Process process = run.exec(cmd);
-			BufferedReader stream_reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			String line;
-			while ((line = stream_reader.readLine()) != null) {
-				LOGGER.log( Level.INFO, "> " + line); //$NON-NLS-1$
+			try (BufferedReader streamReader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+				String line;
+				while ((line = streamReader.readLine()) != null) {
+					LOGGER.log(Level.INFO, "> " + line); //$NON-NLS-1$
+				}
 			}
-			stream_reader.close();
-			process.waitFor();
+			try {
+				process.waitFor();
+			} catch (InterruptedException e) {
+				LOGGER.log(Level.ERROR, e.getMessage(), e);
+			    Thread.currentThread().interrupt();
+			}
 		} catch (IOException e) {
-			e.printStackTrace();
+			LOGGER.log(Level.ERROR, e.getMessage(), e);
 			return SourceMeterStatus.ERROR;
-		} catch (InterruptedException e) {
-			e.printStackTrace();
 		}
 		return SourceMeterStatus.OK;
 	}
 
-	public Hashtable<String, String> getMetrics(int id) {
-		Hashtable<String, String> table = new Hashtable<String, String>();
-		for(Entry<String, String[]> entry : results.entrySet()){
-			table.put(entry.getKey(), entry.getValue()[id]);
+	/**
+	 * Returns the metric at the given index
+	 * 
+	 * @param index The index of the metric
+	 * @return A mapping between the class names and the according values of the
+	 *         requested metric
+	 */
+	public HashMap<String, String> getMetrics(int index) {
+		HashMap<String, String> table = new HashMap<String, String>();
+		for (Entry<String, String[]> entry : results.entrySet()) {
+			table.put(entry.getKey(), entry.getValue()[index]);
 		}
 		return table;
 	}
 
-	public Hashtable<String, String> getMetrics(SourcemeterMetricKeys key) {
+	/**
+	 * Returns the metric at the given key
+	 * 
+	 * @param index The key of the metric
+	 * @return A mapping between the class names and the according values of the
+	 *         requested metric
+	 */
+	public HashMap<String, String> getMetrics(SourcemeterMetricKeys key) {
 		return getMetrics(key.getRow());
 	}
 
-	public double getMetric(String fullyqualified_name, int id) {
-		String resolved_name = ""; //$NON-NLS-1$
-		String[] split = fullyqualified_name.split("\\."); //$NON-NLS-1$
+	/**
+	 * Returns the metric value of a specific class for the metric at the given
+	 * index
+	 * 
+	 * @param fullyqualifiedName The fully qualified name of the class
+	 * @param index              The index of the metric
+	 * @return A mapping between the class names and the according values of the
+	 *         requested metric
+	 * @throws UnknownClassException If the class cannot be found in the results
+	 */
+	public double getMetric(String fullyqualifiedName, int index) throws UnknownClassException {
+		String resolvedName = ""; //$NON-NLS-1$
+		String[] split = fullyqualifiedName.split("\\."); //$NON-NLS-1$
 		for (int i = 0; i < split.length; i++) {
 			int percent = split[i].indexOf('%');
 			if (percent >= 0) {
 				String[] s = split[i].split("%");
 				int j = 0;
-				while(j<s.length){
-					resolved_name += s[j];
-					j+=2;
+				while (j < s.length) {
+					resolvedName += s[j];
+					j += 2;
 				}
 			} else {
-				resolved_name += split[i];
+				resolvedName += split[i];
 			}
 			if (i < split.length - 1) {
-				resolved_name += '.';
+				resolvedName += '.';
 			}
 		}
 
-		if(resolved_name.startsWith("(default package).")){
-			resolved_name = resolved_name.substring("(default package).".length());
+		if (resolvedName.startsWith("(default package).")) {
+			resolvedName = resolvedName.substring("(default package).".length());
 		}
-		
-		String[] values = this.results.get(resolved_name);
+
+		String[] values = this.results.get(resolvedName);
 
 		if (values == null) {
-			if(this.unqualified.containsKey(resolved_name)){
-				String string = this.unqualified.get(resolved_name);
-				if(this.results.containsKey(string)){
+			if (this.unqualified.containsKey(resolvedName)) {
+				String string = this.unqualified.get(resolvedName);
+				if (this.results.containsKey(string)) {
 					values = this.results.get(string);
+				} else {
+					throw new UnknownClassException(resolvedName, string);
 				}
-				else{
-					throw new RuntimeException("Unknown class: \""+resolved_name+"\" with qualified name \""+string+"\"");
-				}
-			}
-			else{
-				throw new RuntimeException("Unknown class: \""+resolved_name+"\"");
+			} else {
+				throw new UnknownClassException("Unknown class: \"" + resolvedName + "\"");
 			}
 		}
-		return Double.parseDouble(values[id]);
+		return Double.parseDouble(values[index]);
 	}
 
-	public double getMetric(String fullyQualifiedName, SourcemeterMetricKeys key) {
+	/**
+	 * Returns the metric value of a specific class for a metric
+	 * 
+	 * @param fullyqualifiedName The fully qualified name of the class
+	 * @param key                The key of the metric
+	 * @return A mapping between the class names and the according values of the
+	 *         requested metric
+	 * @throws UnknownClassException If the class cannot be found in the results
+	 */
+	public double getMetric(String fullyQualifiedName, SourcemeterMetricKeys key) throws UnknownClassException {
 		return getMetric(fullyQualifiedName, key.getRow());
 	}
 
-	private void clear(File file) {
+	private boolean clear(File file) {
+		boolean success = true;
 		if (file.exists()) {
 			for (File f : file.listFiles()) {
 				if (f.isDirectory()) {
-					clear(f);
-					f.delete();
+					success &= (clear(f) && f.delete());
 				} else {
-					f.delete();
+					success &= f.delete();
 				}
 			}
 		}
+		return success;
 	}
 }
--- implementation/org.gravity.metrics.sourcemeter/src/org/gravity/metrics/sourcemeter/MetricPrinter.java
+++ implementation/org.gravity.metrics.sourcemeter/src/org/gravity/metrics/sourcemeter/MetricPrinter.java
@@ -3,6 +3,8 @@ package org.gravity.metrics.sourcemeter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Hashtable;
 
 import org.eclipse.core.resources.IProject;
@@ -16,28 +18,30 @@ public class MetricPrinter {
 		MetricCalculator metrics = new MetricCalculator();
 		SourceMeterStatus status = metrics.calculateMetrics(project.getLocation().toFile(),
 				new File(folder, "sourcemeter"));
+		
 		if (SourceMeterStatus.OK.equals(status)) {
-			Hashtable<String, String> lcom5 = metrics.getMetrics(SourcemeterMetricKeys.LCOM5);
-			double avgLCOM5 = 0;
-			for (String value : lcom5.values()) {
-				avgLCOM5 += Double.valueOf(value);
-			}
-			avgLCOM5 = avgLCOM5 / lcom5.size();
-			results.put("lcom", Double.toString(avgLCOM5));
-			Hashtable<String, String> cbo = metrics.getMetrics(SourcemeterMetricKeys.CBO);
-			double avgCBO = 0;
-			for (String value : cbo.values()) {
-				avgCBO += Double.valueOf(value);
-			}
-			avgCBO = avgCBO / cbo.size();
-			results.put("cbo", Double.toString(avgCBO));
+			HashMap<String, String> lcom5 = metrics.getMetrics(SourcemeterMetricKeys.LCOM5);
+			String avgLcom = average(lcom5.values());
+			results.put("lcom", avgLcom);
+			HashMap<String, String> cbo = metrics.getMetrics(SourcemeterMetricKeys.CBO);
+			String avgCbo = average(cbo.values());
+			results.put("cbo", avgCbo);
 			try {
 				Files.write(new File(folder, "avgMetrics.txt").toPath(),
-						("avg lcom5 = " + avgLCOM5 + "\navg CBO = " + avgCBO).getBytes());
+						("avg lcom5 = " + avgLcom + "\navg CBO = " + avgCbo).getBytes());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 		return results;
+	}
+
+	private static String average(Collection<String> values) {
+		double average = 0;
+		for (String value : values) {
+			average += Double.valueOf(value);
+		}
+		average = average / values.size();
+		return Double.toString(average);
 	}
 }
