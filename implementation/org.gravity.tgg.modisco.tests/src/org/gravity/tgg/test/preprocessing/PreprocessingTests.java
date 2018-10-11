a2df756452499e88816fd729439011c9c568380d
Sven Peldszus <speldszus@uni-koblenz.de>
2018-10-11 14:44:34 +0200

    Removed blocking Bug and improved overall quality

--- implementation/org.gravity.tgg.modisco.tests/src/org/gravity/tgg/test/preprocessing/PreprocessingTests.java
+++ implementation/org.gravity.tgg.modisco.tests/src/org/gravity/tgg/test/preprocessing/PreprocessingTests.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.compare.Diff;
@@ -35,54 +36,83 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class PreprocessingTests {
 
-	public ToFileLogger logToFile = new ToFileLogger(Paths.get(new File("testlogs").toURI()));
+	private static final ToFileLogger TESTLOG = new ToFileLogger(Paths.get(new File("testlogs").toURI()));
 
-	public RetryTest retry = new RetryTest(10);
+	private static final RetryTest RETRY = new RetryTest(10);
 
 	@Rule
-	public TestRule ruleChain = RuleChain.outerRule(this.logToFile).around(this.retry);
+	public TestRule ruleChain = RuleChain.outerRule(TESTLOG).around(RETRY);
 
 	@Parameters(name = "{index}: Preprocessing: {0}")
 	public static Iterable<Object[]> data() {
 		final List<Object[]> testcases = new ArrayList<>();
 
+		final Path testFolder = Paths.get(new File("Testcases/preprocessing").toURI());
+		final Path xmiFolder = testFolder.resolve("AsXMI");
+		final Path expectedFolder = testFolder.resolve("ExpectedResults");
+		final Path logFolder = testFolder.resolve("CreatedOutput");
+
 		try {
-			final Path testFolder = Paths.get(new File("Testcases/preprocessing").toURI());
-			final Path xmiFolder = testFolder.resolve("AsXMI");
-			final Path expectedFolder = testFolder.resolve("ExpectedResults");
-			final Path logFolder = testFolder.resolve("CreatedOutput");
-
 			Files.createDirectories(logFolder);
-
-			// collect xmi testcases
-
-			final List<Path> detectedTestcases = Files.walk(xmiFolder)
-					.filter(path -> PathUtil.pathEndsWith(path, ".xmi")).collect(toList());
-
-			final LinkedList<Path> detectedExpectedResults = Files.walk(expectedFolder)
-					.filter(path -> PathUtil.pathEndsWith(path, ".xmi"))
-					.collect(Collectors.toCollection(LinkedList::new));
-
-			final List<TestBody> finalCases = detectedTestcases.stream()
-					.map(testcase -> PathUtil.createTestcase(testcase, detectedExpectedResults, logFolder))
-					.collect(toList());
-
-			for (final TestBody testcase : finalCases) {
-				testcases.add(new Object[] { testcase.getTestName(), testcase });
-			}
-
 		} catch (final IOException e) {
 			throw new IllegalArgumentException(e);
+		}
+
+		// collect xmi testcases
+		final LinkedList<Path> detectedExpectedResults = getXMIFiles(expectedFolder);
+		final List<TestBody> finalCases = getTestBodies(xmiFolder, logFolder, detectedExpectedResults);
+
+		for (final TestBody testcase : finalCases) {
+			testcases.add(new Object[] { testcase.getTestName(), testcase });
 		}
 
 		return testcases;
 	}
 
-	private final String fileName;
+	/**
+	 * Crates test bodies from the detected test files
+	 * 
+	 * @param xmiFolder The folder with the input models
+	 * @param logFolder The folder to which should be logged
+	 * @param detectedExpectedResults The folder with the expected results
+	 * @return
+	 */
+	private static List<TestBody> getTestBodies(final Path xmiFolder, final Path logFolder,
+			final LinkedList<Path> detectedExpectedResults) {
+		final List<TestBody> finalCases;
+		try (Stream<Path> xmiWalk = Files.walk(xmiFolder)) {
+			finalCases = xmiWalk.filter(path -> PathUtil.pathEndsWith(path, ".xmi"))
+					.map(testcase -> PathUtil.createTestcase(testcase, detectedExpectedResults, logFolder))
+					.collect(toList());
+
+		} catch (final IOException e) {
+			throw new IllegalArgumentException(e);
+		}
+		return finalCases;
+	}
+
+	/**
+	 * Searches all XMI files in the given folder
+	 * 
+	 * @param folder The folder to search in
+	 * @return The XMI files
+	 */
+	private static LinkedList<Path> getXMIFiles(final Path folder) {
+		final LinkedList<Path> detectedExpectedResults;
+		try (Stream<Path> expectedWalk = Files.walk(folder)) {
+
+			detectedExpectedResults = expectedWalk.filter(path -> PathUtil.pathEndsWith(path, ".xmi"))
+					.collect(Collectors.toCollection(LinkedList::new));
+
+		} catch (final IOException e) {
+			throw new IllegalArgumentException(e);
+		}
+		return detectedExpectedResults;
+	}
+
 	private final TestBody testcase;
 
 	public PreprocessingTests(final String fileName, final TestBody testcase) {
-		this.fileName = fileName;
 		this.testcase = testcase;
 	}
 
