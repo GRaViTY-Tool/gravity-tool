1d4a8ab16125853f0ef35e62226f07dc87892d7e
Sven Peldszus <speldszus@uni-koblenz.de>
2018-10-12 10:24:51 +0200

    Replaced use of System.err by setting message and added Javadoc

--- implementation/org.gravity.typegraph.basic/src/org/gravity/typegraph/basic/compare/UnsupportetElementException.java
+++ implementation/org.gravity.typegraph.basic/src/org/gravity/typegraph/basic/compare/UnsupportetElementException.java
@@ -1,21 +1,33 @@
 package org.gravity.typegraph.basic.compare;
 
+/**
+ * This exception should be thrown if an element is not supported by a processing of a model
+ * 
+ * @author speldszus
+ *
+ */
 public class UnsupportetElementException extends Exception {
 
 	private static final long serialVersionUID = -5817533255729198889L;
 
 	private final Class<?> type;
 
+	/**
+	 * A constructor taking the unsupported element
+	 * 
+	 * @param type The unsupported element
+	 */
 	public UnsupportetElementException(Class<?> type) {
+		super("The type \"" + type + "\" is not supported"); //$NON-NLS-1$//$NON-NLS-2$
 		this.type = type;
 	}
 
+	/**
+	 * A getter for the unsupported element due to which this exception has been thrown
+	 * 
+	 * @return the unsupported element
+	 */
 	public Class<?> getUnsupportetType() {
 		return this.type;
-	}
-
-	@Override
-	public void printStackTrace() {
-		System.err.print("The type \"" + this.type + "\" is not supported"); //$NON-NLS-1$//$NON-NLS-2$
 	}
 }
