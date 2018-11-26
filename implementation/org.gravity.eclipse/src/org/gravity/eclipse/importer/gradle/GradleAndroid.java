c8c3d097c51a3b00db8186cd85a786f96ab158c8
Sven Peldszus <speldszus@uni-koblenz.de>
2018-11-26 14:57:31 +0100

    Added LOGGER

--- implementation/org.gravity.eclipse/src/org/gravity/eclipse/importer/gradle/GradleAndroid.java
+++ implementation/org.gravity.eclipse/src/org/gravity/eclipse/importer/gradle/GradleAndroid.java
@@ -10,6 +10,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -22,6 +23,8 @@ import org.xml.sax.SAXException;
  *
  */
 public class GradleAndroid {
+
+	private static final Logger LOGGER = Logger.getLogger(GradleAndroid.class);
 
 	/**
 	 * Searches for the generated R.java class in the gradle project
@@ -43,10 +46,10 @@ public class GradleAndroid {
 						if (rFile.exists()) {
 							classes.add(rFile.toPath());
 						} else {
-							GradleImport.LOGGER.log(Level.WARN, "The R.java does not exist: " + rFile.getAbsolutePath());
+							LOGGER.log(Level.WARN, "The R.java does not exist: " + rFile.getAbsolutePath());
 						}
 					} else {
-						GradleImport.LOGGER.log(Level.WARN, "No R.java file found");
+						LOGGER.log(Level.WARN, "No R.java file found");
 					}
 				}
 			}
@@ -65,13 +68,13 @@ public class GradleAndroid {
 			File releaseFolder;
 			if (!(releaseFolder = new File(rFolder, "release")).exists()) {
 				if (!(releaseFolder = new File(rFolder, "debug")).exists()) {
-					GradleImport.LOGGER.log(Level.WARN, "No \"release\" or \"debug\" folder in \"" + rFolder + "\"");
+					LOGGER.log(Level.WARN, "No \"release\" or \"debug\" folder in \"" + rFolder + "\"");
 					return null;
 				}
 			}
 			return new File(new File(releaseFolder, basePackage.replace('.', '/')), "R.java");
 		} catch (ParserConfigurationException | SAXException e) {
-			GradleImport.LOGGER.log(Level.ERROR, e);
+			LOGGER.log(Level.ERROR, e);
 		}
 		return null;
 	}
