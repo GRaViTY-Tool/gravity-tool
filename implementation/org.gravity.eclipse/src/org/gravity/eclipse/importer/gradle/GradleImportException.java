35c1bff7b36f0403af03aa343458d90d65ee8fdc
Sven Peldszus <speldszus@uni-koblenz.de>
2018-11-26 14:27:03 +0100

    Added JavaDoc

--- implementation/org.gravity.eclipse/src/org/gravity/eclipse/importer/gradle/GradleImportException.java
+++ implementation/org.gravity.eclipse/src/org/gravity/eclipse/importer/gradle/GradleImportException.java
@@ -1,22 +1,44 @@
 package org.gravity.eclipse.importer.gradle;
 
+/**
+ * This exception should be thrwon if the import of a gradle project failed
+ * 
+ * @author speldszus
+ *
+ */
 public class GradleImportException extends Exception {
 
+	/**
+	 * A generated serial version ID 
+	 */
+	private static final long serialVersionUID = -300723204808288520L;
+	
+	/**
+	 * Creates an instance with an given message
+	 * 
+	 * @param message The message
+	 */
 	public GradleImportException(String message) {
 		super(message);
 	}
 
+	/**
+	 * Creates an instance with an other exception as cause and a given message
+	 * 
+	 * @param message The message
+	 * @param cause The cause
+	 */
 	public GradleImportException(String message, Exception cause) {
 		super(message, cause);
 	}
 
+	/**
+	 * Creates an instance with an other exception as cause
+	 * 
+	 * @param cause The cause
+	 */
 	public GradleImportException(Exception cause) {
 		super(cause);
 	}
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -300723204808288520L;
 
 }
--- implementation/org.gravity.eclipse/src/org/gravity/eclipse/importer/gradle/NoGradleRootFolderException.java
+++ implementation/org.gravity.eclipse/src/org/gravity/eclipse/importer/gradle/NoGradleRootFolderException.java
@@ -1,14 +1,22 @@
 package org.gravity.eclipse.importer.gradle;
 
+/**
+ * A Exception of this type should be thrown if the root of a gradle project cannot be found
+ * @author speldszus
+ *
+ */
 public class NoGradleRootFolderException extends GradleImportException {
 
+	/**
+	 * A generated id
+	 */
+	private static final long serialVersionUID = 5578756709979719901L;
+
+	/**
+	 * Creates a new Exception with a default message
+	 */
 	public NoGradleRootFolderException() {
 		super("The gradle root folder cannot be found.");
 	}
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 5578756709979719901L;
 
 }
