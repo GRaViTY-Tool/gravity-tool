113fd875334dc276879d1016bb37fc931dba30f5
Sven Peldszus <speldszus@uni-koblenz.de>
2018-12-17 16:04:30 +0100

    Fixed initialization of relative detectors and improved code quality

--- implementation/org.gravity.hulk/src/org/gravity/hulk/detection/HulkDetector.java
+++ implementation/org.gravity.hulk/src/org/gravity/hulk/detection/HulkDetector.java
@@ -38,30 +38,30 @@ import org.moflon.core.dfs.Node;
  */
 public class HulkDetector {
 
-	private static final Logger LOGGER = Logger.getLogger( HulkDetector.class.getName() );
-	
+	private static final Logger LOGGER = Logger.getLogger(HulkDetector.class.getName());
+
 	private Hashtable<String, String> thresholds;
 	private HAntiPatternHandling hulk;
 	private Set<HDetector> initialized;
-	
+
 	private boolean verbose;
 
 	/**
 	 * Creates a new detection instance disabling verbose
 	 * 
-	 * @param hulk The detection configuration
+	 * @param hulk       The detection configuration
 	 * @param thresholds The thresholds for the detection
 	 */
-	public HulkDetector(HAntiPatternHandling hulk, Hashtable<String, String> thresholds){
+	public HulkDetector(HAntiPatternHandling hulk, Hashtable<String, String> thresholds) {
 		this(hulk, thresholds, false);
 	}
-	
+
 	/**
 	 * Creates a new detection instance
 	 * 
-	 * @param hulk The detection configuration
+	 * @param hulk       The detection configuration
 	 * @param thresholds The thresholds for the detection
-	 * @param verbose The verbose state
+	 * @param verbose    The verbose state
 	 */
 	public HulkDetector(HAntiPatternHandling hulk, Hashtable<String, String> thresholds, boolean verbose) {
 		this.hulk = hulk;
@@ -69,7 +69,7 @@ public class HulkDetector {
 		this.verbose = verbose;
 		initialized = new HashSet<>();
 		Resource eResource = hulk.eResource();
-		if(eResource == null) {
+		if (eResource == null) {
 			eResource = new ResourceSetImpl().createResource(URI.createURI("Hulk"));
 			eResource.getContents().add(hulk);
 		}
@@ -98,82 +98,94 @@ public class HulkDetector {
 		return sorted;
 	}
 
-	private void handleDetector(HDetector detector, Stack<HDetector> worklist, Set<HDetector> processed_detectors,
+	private void handleDetector(HDetector detector, Stack<HDetector> worklist, Set<HDetector> processedDetectors,
 			boolean verbose) throws DetectionFailedException {
 		List<HDetector> sorted = getSorted(detector);
-		for (HDetector n : sorted) {
+		for (HDetector nextDetector : sorted) {
 			long t2 = 0;
-			if (!processed_detectors.contains(n)) {
-				if (worklist.contains(n)) {
-					worklist.remove(n);
+			if (!processedDetectors.contains(nextDetector)) {
+				if (worklist.contains(nextDetector)) {
+					worklist.remove(nextDetector);
 				}
 				if (verbose) {
 					t2 = System.currentTimeMillis();
-					LOGGER.log( Level.INFO, t2 + " Hulk " + n.getGuiName());
+					LOGGER.log(Level.INFO, t2 + " Hulk " + nextDetector.getGuiName());
 				}
-				if (n instanceof HRelativeDetector) {
-					HRelativeDetector relativeDetector = (HRelativeDetector) n;
-					String key = relativeDetector.getClass().getName().replace("Impl", "").replace(".impl", "");
-					if (thresholds.containsKey(key)) {
-						if (!initialized.contains(detector)) {
-							relativeDetector.setRelative(false);
-							String value = thresholds.get(key);
-							HRelativeValueConstants constant = HRelativeValueConstants.getByName(value);
-							if (constant != null) {
-								relativeDetector.setThreshold(relativeDetector.calculateRelativeThreshold(constant));
-							} else {
-								Double number = Double.valueOf(value);
-								if (number != null) {
-									relativeDetector.setThreshold(number.doubleValue());
-								} else {
-									throw new DetectionFailedException("The stored threshold of the metric \""+ key
-											+ "\" is not a double!");
-								}
-							}
-						}
-					}
+				if (nextDetector instanceof HRelativeDetector) {
+					initializeRelativeDetector((HRelativeDetector) nextDetector);
 				}
-				if (n.detect(hulk.getApg())) {
-					n.setPostTraversal(0);
-					n.setPreTraversal(0);
-					processed_detectors.add(n);
+				if (nextDetector.detect(hulk.getApg())) {
+					nextDetector.setPostTraversal(0);
+					nextDetector.setPreTraversal(0);
+					processedDetectors.add(nextDetector);
 				} else {
-					LOGGER.log(Level.ERROR, Messages.getString("detector.failed") + n); //$NON-NLS-1$
+					LOGGER.log(Level.ERROR, Messages.getString("detector.failed") + nextDetector); //$NON-NLS-1$
 				}
 				if (verbose) {
 					long t3 = System.currentTimeMillis();
-					LOGGER.log( Level.INFO, t3 + " Hulk " + n.getGuiName() + " - done " + (t3 - t2) + "ms");
+					LOGGER.log(Level.INFO, t3 + " Hulk " + nextDetector.getGuiName() + " - done " + (t3 - t2) + "ms");
 				}
 			}
 		}
 	}
 
-	private boolean detectSelectedAntiPattern(Stack<HDetector> worklist, Set<HDetector> processed_detectors,
+	/**
+	 * Initializes the thresholds of the relative detector
+	 * 
+	 * @param relativeDetector The detector
+	 * @throws DetectionFailedException If the stored threshold is not valid
+	 */
+	private void initializeRelativeDetector(HRelativeDetector relativeDetector) throws DetectionFailedException {
+		if (!initialized.contains(relativeDetector)) {
+			String key = relativeDetector.getClass().getName().replace("Impl", "").replace(".impl", "");
+			if (thresholds.containsKey(key)) {
+				relativeDetector.setRelative(false);
+				String value = thresholds.get(key);
+				HRelativeValueConstants constant = HRelativeValueConstants.getByName(value);
+				if (constant != null) {
+					relativeDetector.setThreshold(relativeDetector.calculateRelativeThreshold(constant));
+				} else {
+					Double number = Double.valueOf(value);
+					if (number != null) {
+						relativeDetector.setThreshold(number.doubleValue());
+					} else {
+						throw new DetectionFailedException(
+								"The stored threshold of the metric \"" + key + "\" is not a double!");
+					}
+				}
+			}
+		}
+	}
+
+	private boolean detectSelectedAntiPattern(Stack<HDetector> worklist, Set<HDetector> processedDetectors,
 			boolean verbose) throws DetectionFailedException {
 		long h0 = 0;
 		if (verbose) {
 			h0 = System.currentTimeMillis();
-			LOGGER.log( Level.INFO, h0 + " Hulk Anti-Pattern Detection");
+			LOGGER.log(Level.INFO, h0 + " Hulk Anti-Pattern Detection");
 		}
 		while (!worklist.isEmpty()) {
 			HDetector detector = worklist.pop();
 
-			handleDetector(detector, worklist, processed_detectors, verbose);
+			handleDetector(detector, worklist, processedDetectors, verbose);
 		}
 		if (verbose) {
 			long h1 = System.currentTimeMillis();
-			LOGGER.log( Level.INFO, h1 + " Hulk Anti-Pattern Detection - done " + (h1 - h0) + "ms");
+			LOGGER.log(Level.INFO, h1 + " Hulk Anti-Pattern Detection - done " + (h1 - h0) + "ms");
 		}
 
 		return true;
 	}
 
 	/**
-	 * Executes the anti-pattern detection with the selected detectors on the loaded program model
+	 * Executes the anti-pattern detection with the selected detectors on the loaded
+	 * program model
 	 * 
-	 * @param selection The types of the selected detectors
-	 * @param selectedDetectors This set will contain the instances of the selected detectors after detection.
-	 * @param processedDetectors This set will contain the instances of all executed detectors after detection.
+	 * @param selection          The types of the selected detectors
+	 * @param selectedDetectors  This set will contain the instances of the selected
+	 *                           detectors after detection.
+	 * @param processedDetectors This set will contain the instances of all executed
+	 *                           detectors after detection.
 	 * @return true, iff the detection has been performed successfully
 	 */
 	public boolean detectSelectedAntiPattern(Set<EClass> selection, Set<HDetector> selectedDetectors,
