5df3937162185f1cf44741a4edbc33bacbddf539
Sven Peldszus <speldszus@uni-koblenz.de>
2018-12-19 13:04:20 +0100

    Updated messages

--- implementation/org.gravity.eclipse/src/org/gravity/eclipse/Messages.java
+++ implementation/org.gravity.eclipse/src/org/gravity/eclipse/Messages.java
@@ -6,54 +6,55 @@ import org.eclipse.osgi.util.NLS;
  * The Class Messages.
  */
 public class Messages extends NLS {
+	
+	/** The Constant BUNDLENAME. */
+	public static final String BUNDLENAME = "org.gravity.eclipse.messages"; //$NON-NLS-1$
+	
+	/** The Java parse handler0. */
+	public static String javaParseHandler0;
+	
+	/** The Java parse handler1. */
+	public static String javaParseHandler1;
+	
+	/** The Java parse handler2. */
+	public static String javaParseHandler2;
+	
+	/** The Java source to emf converter0. */
+	public static String javaSourceToEMFConverter0;
+	
+	/** The Java source to emf converter1. */
+	public static String javaSourceToEMFConverter1;
+	
+	/** The Java source to emf converter2. */
+	public static String javaSourceToEMFConverter2;
+	
+	/** The Java source to emf converter3. */
+	public static String javaSourceToEMFConverter3;
+	
+	/** The Java source to emf converter4. */
+	public static String javaSourceToEMFConverter4;
+	
+	/** The Java source to emf converter5. */
+	public static String javaSourceToEMFConverter5;
+	
+	/** The Java source to emf converter6. */
+	public static String javaSourceToEMFConverter6;
+	
+	/** The Java source to emf converter7. */
+	public static String javaSourceToEMFConverter7;
+	
+	/** The Typed tree node0. */
+	public static String typedTreeNode0;
+
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLENAME, Messages.class);
+	}
 
 	/**
 	 * Instantiates a new messages.
 	 */
 	private Messages() {
-	}
-	
-	/** The Constant BUNDLENAME. */
-	private static final String BUNDLENAME = "org.gravity.eclipse.messages"; //$NON-NLS-1$
-	
-	/** The Java parse handler0. */
-	public static String JavaParseHandler0;
-	
-	/** The Java parse handler1. */
-	public static String JavaParseHandler1;
-	
-	/** The Java parse handler2. */
-	public static String JavaParseHandler2;
-	
-	/** The Java source to emf converter0. */
-	public static String JavaSourceToEMFConverter0;
-	
-	/** The Java source to emf converter1. */
-	public static String JavaSourceToEMFConverter1;
-	
-	/** The Java source to emf converter2. */
-	public static String JavaSourceToEMFConverter2;
-	
-	/** The Java source to emf converter3. */
-	public static String JavaSourceToEMFConverter3;
-	
-	/** The Java source to emf converter4. */
-	public static String JavaSourceToEMFConverter4;
-	
-	/** The Java source to emf converter5. */
-	public static String JavaSourceToEMFConverter5;
-	
-	/** The Java source to emf converter6. */
-	public static String JavaSourceToEMFConverter6;
-	
-	/** The Java source to emf converter7. */
-	public static String JavaSourceToEMFConverter7;
-	
-	/** The Typed tree node0. */
-	public static String TypedTreeNode0;
-
-	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLENAME, Messages.class);
+		// There shouldn't be any instance of this class
 	}
 }
