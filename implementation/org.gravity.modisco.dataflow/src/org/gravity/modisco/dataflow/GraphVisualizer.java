3f031c72cbbba396c2c8150799913527186c16aa
dmebus <dmebus@uni-koblenz.de>
2019-06-06 15:35:28 +0200

    Fixed findings in GraphVisualizer

--- implementation/org.gravity.modisco.dataflow/src/org/gravity/modisco/dataflow/GraphVisualizer.java
+++ implementation/org.gravity.modisco.dataflow/src/org/gravity/modisco/dataflow/GraphVisualizer.java
@@ -9,6 +9,8 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmt.modisco.java.MethodInvocation;
 import org.eclipse.gmt.modisco.java.NamedElement;
@@ -22,14 +24,29 @@ import guru.nidi.graphviz.engine.Graphviz;
 import guru.nidi.graphviz.model.MutableGraph;
 import guru.nidi.graphviz.model.MutableNode;
 
+/**
+ * Functionality to draw dot graphs with intra-procedural data flow edges for method/field definitions of Java programs.
+ * The drawn graphs are based on the program representations created by the data flow handlers.
+ * 
+ * @author dmebus
+ *
+ */
 public class GraphVisualizer {
 	
-	// TODO Counter or project name to alternate file name?
+	private static final Logger LOGGER = Logger.getLogger(GraphVisualizer.class);
 	
+	private static int j = 0;
+	
+	/**
+	 * Draws the dot graphs for each StatementHandlerDataFlow instance in the given list.
+	 * The graphs are exported as PNG image files.
+	 * 
+	 * @param handlers The list of StatementHandlerDataFlow instances, for which a graph is supposed to be drawn.
+	 */
 	public static void drawGraphs(List<StatementHandlerDataFlow> handlers) {
 		int i = 0;
 		for (StatementHandlerDataFlow handler : handlers) {
-			MutableGraph g = mutGraph("example1").setDirected(true).graphAttrs().add(RankDir.TOP_TO_BOTTOM).graphAttrs().add("dpi", 72);
+			MutableGraph g = mutGraph("graph" + i).setDirected(true).graphAttrs().add(RankDir.TOP_TO_BOTTOM).graphAttrs().add("dpi", 72);
 			HashMap<FlowNode, MutableNode> graphNodes = new HashMap<>();
 			// Creating a graph node for each FlowNode
 			Collection<FlowNode> alreadySeenNodes = handler.getAlreadySeen().values();
@@ -60,15 +77,22 @@ public class GraphVisualizer {
 				}
 			}
 			try {
-				Graphviz.fromGraph(g).width(10000).render(Format.PNG).toFile(new File("example" + File.separator + "ex" + i + ".png"));
-				System.out.println("Created dot graph");
+				Graphviz.fromGraph(g).width(10000).render(Format.PNG).toFile(new File("graphs" + File.separator + "project" + j + File.separator + "graph" + i + ".png"));
 			} catch (IOException e) {
-				e.printStackTrace();
+				LOGGER.log(Level.ERROR, e.getMessage(), e);
 			}
 			i++;
 		}
+		j++;
 	}
 
+	/**
+	 * Returns a (new) dot graph node for the given FlowNode. 
+	 * If the node already exists, the existing one is returned (by the mutNode constructor), so duplicates are prevented.
+	 * 
+	 * @param node The FlowNode, for which a dot graph node is requested.
+	 * @return The requested dot graph node, which corresponds to the given FlowNode.
+	 */
 	private static MutableNode getDotNode(FlowNode node) {
 		EObject modelElement = node.getModelElement();
 		String label = modelElement.eClass().getName();
