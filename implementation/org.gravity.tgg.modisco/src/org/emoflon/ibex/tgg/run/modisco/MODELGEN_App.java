50ea6951cc706fc1fe1aae67f6006c838be7541a
Sven Peldszus <speldszus@uni-koblenz.de>
2019-06-11 17:42:10 +0200

    Added eMoflon model gen

---
+++ implementation/org.gravity.tgg.modisco/src/org/emoflon/ibex/tgg/run/modisco/MODELGEN_App.java
@@ -0,0 +1,56 @@
+package org.emoflon.ibex.tgg.run.modisco;
+
+import java.io.IOException;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+
+import org.emoflon.ibex.tgg.operational.defaults.IbexOptions;
+import org.emoflon.ibex.tgg.operational.strategies.gen.MODELGEN;
+import org.emoflon.ibex.tgg.operational.strategies.gen.MODELGENStopCriterion;
+import org.emoflon.ibex.tgg.runtime.engine.DemoclesTGGEngine;
+
+public class MODELGEN_App extends MODELGEN {
+
+	public MODELGEN_App() throws IOException {
+		super(createIbexOptions());
+		registerBlackInterpreter(new DemoclesTGGEngine());
+	}
+
+	public static void main(String[] args) throws IOException {
+		BasicConfigurator.configure();
+		Logger.getRootLogger().setLevel(Level.INFO);
+
+		logger.info("Starting MODELGEN");
+		long tic = System.currentTimeMillis();
+		MODELGEN_App generator = new MODELGEN_App();
+		long toc = System.currentTimeMillis();
+		logger.info("Completed init for MODELGEN in: " + (toc - tic) + " ms");
+		
+		MODELGENStopCriterion stop = new MODELGENStopCriterion(generator.getTGG());
+		stop.setTimeOutInMS(1000);
+		generator.setStopCriterion(stop);
+		
+		tic = System.currentTimeMillis();
+		generator.run();
+		toc = System.currentTimeMillis();
+		logger.info("Completed MODELGEN in: " + (toc - tic) + " ms");
+		
+		generator.saveModels();
+		generator.terminate();
+	}
+	
+	
+	@Override
+	protected void registerUserMetamodels() throws IOException {
+		_RegistrationHelper.registerMetamodels(rs, this);
+			
+		// Register correspondence metamodel last
+		loadAndRegisterCorrMetamodel(options.projectPath() + "/model/" + options.projectName() + ".ecore");
+	}
+	
+	private static IbexOptions createIbexOptions() {
+		return _RegistrationHelper.createIbexOptions();
+	}
+}
