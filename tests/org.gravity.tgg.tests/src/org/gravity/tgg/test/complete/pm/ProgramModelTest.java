12b7c2ab6a453a29d8c46a45c0b98a6b33f30cf8
dmebus <dmebus@uni-koblenz.de>
2019-08-05 22:02:26 +0200

    Checking numbers of generated model elements per type

--- tests/org.gravity.tgg.tests/src/org/gravity/tgg/test/complete/pm/ProgramModelTest.java
+++ tests/org.gravity.tgg.tests/src/org/gravity/tgg/test/complete/pm/ProgramModelTest.java
@@ -1,21 +1,32 @@
 package org.gravity.tgg.test.complete.pm;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.net.MalformedURLException;
 import java.util.Collections;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jdt.core.IJavaProject;
 import org.gravity.eclipse.converter.IPGConverter;
 import org.gravity.tgg.modisco.MoDiscoTGGConverter;
 import org.gravity.tgg.test.complete.AbstractParameterizedTransformationTest;
 import org.gravity.tgg.test.util.TimeStampUtil;
 import org.gravity.typegraph.basic.TypeGraph;
+
+import com.github.cliftonlabs.json_simple.JsonException;
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsoner;
 
 /**
  * 
@@ -69,12 +80,35 @@ public class ProgramModelTest extends AbstractParameterizedTransformationTest {
 				pg.eResource().save(new FileOutputStream("outPM/" + pg.getTName() + ".xmi"), Collections.emptyMap());
 				assertNotNull(pg);
 				
+				// Check, if element counts (e. g. number of TFlows) are as expected
+				IPath path = project.getProject().getLocation().addTrailingSeparator().append("expect.json");
+				File file = new File(path.toString());
+				if (file.exists()) {
+					FileReader fileReader = new FileReader(file);
+					JsonObject map = (JsonObject) Jsoner.deserialize(fileReader);
+					TreeIterator<EObject> it = pg.eResource().getAllContents();
+					BigDecimal one = new BigDecimal(1);
+					while (it.hasNext()) {
+						EObject e = it.next();
+						e.eClass().getEAllSuperTypes(); // TODO Count super types as well
+						String typeName = e.eClass().getName();
+						if (map.containsKey(typeName)) {
+							BigDecimal count = (BigDecimal) map.get(typeName);
+							map.put(typeName,  count.subtract(one));
+						}
+					}
+					for (Object count : map.values()) {
+						assertEquals(0, ((BigDecimal) count).intValue());
+					}
+				}
+				
 			} catch (final MalformedURLException e) {
 				throw new AssertionError(String.format("Unable to load '%s': %s", project, e.getMessage()));
 			} catch (final IOException e) {
 				throw new AssertionError(String.format("Unable to load '%s': %s", project, e.getMessage()));
+			} catch (final JsonException e) {
+				throw new AssertionError(String.format("Unable to load '%s': %s", project, e.getMessage()));
 			}
-
 		} catch (final IllegalArgumentException iae) {
 			throw new AssertionError(String.format("Unable to load '%s': %s", project, iae.getMessage()));
 		}
