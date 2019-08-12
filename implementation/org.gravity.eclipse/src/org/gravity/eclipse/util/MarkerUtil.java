bf14fbc0b68b32198362f28b029b247b173dda2c
Sven Peldszus <speldszus@uni-koblenz.de>
2019-08-12 15:03:27 +0200

    Added util to create and delete markers

---
+++ implementation/org.gravity.eclipse/src/org/gravity/eclipse/util/MarkerUtil.java
@@ -0,0 +1,176 @@
+/**
+ * 
+ */
+package org.gravity.eclipse.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.ISourceReference;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+import org.gravity.typegraph.basic.TAbstractType;
+import org.gravity.typegraph.basic.TMethodDefinition;
+
+/**
+ * @author speldszus
+ *
+ */
+public class MarkerUtil {
+
+	private static final Logger LOGGER = Logger.getLogger(MarkerUtil.class);
+
+	private final Map<String, IType> astTypes;
+	
+	/**
+	 * Initializes the marker util for the given project
+	 * 
+	 * @param project The project in which markers should be created
+	 * @throws JavaModelException If there is an error in accessing contents from the project
+	 */
+	public MarkerUtil(IJavaProject project) throws JavaModelException {
+		astTypes = JavaASTUtil.getTypesForProject(project);
+	}
+
+	
+	/**
+	 * Creates a marker on the given program model element
+	 * 
+	 * @param element  The pm element
+	 * @param kind     The marker kind
+	 * @param message  The marker message
+	 * @param priority The marker priority
+	 * @param severity The severity of the marker
+	 */
+	public void createMarker(EObject element, String message, int priority, int severity) {
+		createMarker(astTypes, element, message, priority, severity);
+	}
+	
+	/**
+	 * Creates a marker on the given program model element
+	 * 
+	 * @param astTypes A mapping between names and Eclipse AST types
+	 * @param element  The pm element
+	 * @param kind     The marker kind
+	 * @param message  The marker message
+	 * @param priority The marker priority
+	 * @param severity The severity of the marker
+	 * 
+	 * @deprecated
+	 */
+	public static void createMarker(Map<String, IType> astTypes, EObject element, String message, int priority, int severity) {
+		IJavaElement javaElement = getJavaElement(astTypes, element);
+		if (javaElement != null) {
+			try {
+				int line = getLine(javaElement);
+
+				IMarker marker = javaElement.getUnderlyingResource().createMarker(IMarker.PROBLEM);
+				marker.setAttribute(IMarker.MESSAGE, message);
+				marker.setAttribute(IMarker.PRIORITY, priority);
+				marker.setAttribute(IMarker.SEVERITY, severity);
+				marker.setAttribute(IMarker.LINE_NUMBER, line);
+			} catch (CoreException e) {
+				LOGGER.log(Level.ERROR, e);
+			}
+		}
+	}
+
+	/**
+	 * Get the element from the astTypes map
+	 * 
+	 * @param astTypes A map from names to all types contained in the JDT AST
+	 * @param element The program model element to look for
+	 * @return The corresponding Java element
+	 */
+	private static IJavaElement getJavaElement(Map<String, IType> astTypes, EObject element) {
+		IJavaElement javaElement = null;
+		if (element instanceof TAbstractType) {
+			javaElement = astTypes.get(((TAbstractType) element).getFullyQualifiedName());
+		} else if (element instanceof TMethodDefinition) {
+			TMethodDefinition member = (TMethodDefinition) element;
+			String fullyQualifiedName = member.getDefinedBy().getFullyQualifiedName();
+			IType iType = astTypes.get(fullyQualifiedName);
+			if(iType == null) {
+				LOGGER.log(Level.WARN, "Type not found in AST: " + fullyQualifiedName);
+				return null;
+			}
+			try {
+				javaElement = JavaASTUtil.getIMethod(member.getSignature(), iType);
+			} catch (JavaModelException e) {
+				LOGGER.log(Level.ERROR, e);
+			}
+		}
+		return javaElement;
+	}
+	
+	/**
+	 * Deleted all markers from the given Element
+	 * @param element The program model element to look for
+	 */
+	public void deleteMarker( EObject element) {
+		deleteMarker(astTypes, element);
+	}
+
+	/**
+	 * Deleted all markers from the given Element
+	 * 
+	 * @param astTypes A map from names to all types contained in the JDT AST
+	 * @param element The program model element to look for
+	 * 
+	 * @deprecated
+	 */
+	public static void deleteMarker(Map<String, IType> astTypes, EObject element) {
+		IJavaElement javaElement = getJavaElement(astTypes, element);
+		if (javaElement != null) {
+			try {
+				int line = getLine(javaElement);
+				for (IMarker marker : javaElement.getUnderlyingResource().findMarkers(IMarker.PROBLEM, true,
+						IResource.DEPTH_ONE)) {
+					Object attribute = marker.getAttribute(IMarker.LINE_NUMBER);
+					if (((Integer) attribute).intValue() == line) {
+						marker.delete();
+					}
+				}
+			} catch (CoreException e) {
+				LOGGER.log(Level.ERROR, e);
+			}
+		}
+	}
+
+	/**
+	 * Get the source code line of the given element
+	 * 
+	 * @param javaElement The element to look for
+	 * @return The source code line
+	 * @throws JavaModelException
+	 * @throws CoreException
+	 */
+	private static int getLine(IJavaElement javaElement) throws JavaModelException, CoreException {
+		IResource underlyingResource = javaElement.getUnderlyingResource();
+		int line = 1;
+		if (underlyingResource.getFileExtension().equals("java")) {
+			try (InputStream stream = ((IFile) underlyingResource).getContents()) {
+				char ch;
+				int count = ((ISourceReference) javaElement).getSourceRange().getOffset();
+				while ((ch = (char) stream.read()) != -1 && count-- > 0) {
+					if (ch == '\n') {
+						line++;
+					}
+				}
+			} catch (IOException e) {
+				LOGGER.log(Level.ERROR, e);
+			}
+		}
+		return line;
+	}
+}
