42bdb89cbf353ebb697ac918ac336d5738e963aa
Sven Peldszus <speldszus@uni-koblenz.de>
2019-08-12 18:13:36 +0200

    Changed test to XMI model as input

--- tests/org.gravity.hulk.tests/src/org/gravity/hulk/tests/metics/AccessAnalysisTest.java
+++ tests/org.gravity.hulk.tests/src/org/gravity/hulk/tests/metics/AccessAnalysisTest.java
@@ -5,36 +5,24 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.jdt.core.IJavaProject;
-import org.gravity.eclipse.GravityAPI;
 import org.gravity.eclipse.exceptions.TransformationFailedException;
 import org.gravity.eclipse.io.GitCloneException;
-import org.gravity.eclipse.io.GitTools;
-import org.gravity.eclipse.tests.TestHelper;
-import org.gravity.eclipse.util.EclipseProjectUtil;
 import org.gravity.hulk.HAntiPatternDetection;
 import org.gravity.hulk.HulkFactory;
 import org.gravity.hulk.antipatterngraph.AntipatterngraphFactory;
@@ -55,6 +43,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import org.osgi.framework.Bundle;
 
 /**
  * A test if our implementation of the IGAM and IGAT metrics return the results
@@ -68,9 +57,10 @@ public class AccessAnalysisTest {
 
 	private static final Logger LOGGER = Logger.getLogger(AccessAnalysisTest.class.getName());
 
-	private IJavaProject javaProject;
-
 	private TypeGraph pmExpect;
+	private TypeGraph pmInput;
+	
+	private static final String[] projects = new String[] {"SecureMailApp"};
 
 	/**
 	 * Creates a new test instance for the given project
@@ -80,12 +70,25 @@ public class AccessAnalysisTest {
 	 * @param The     program model containing the expected annotations
 	 * @throws IOException If the expected program model cannot be loaded
 	 */
-	public AccessAnalysisTest(String name, IJavaProject project, URL pmExpect) throws IOException {
-		this.javaProject = project;
-		Resource resource = new ResourceSetImpl().createResource(URI.createFileURI(pmExpect.getFile()));
-		resource.load(pmExpect.openStream(), Collections.emptyMap());
-		this.pmExpect = (TypeGraph) resource.getContents().get(0);
+	public AccessAnalysisTest(String name, URL pmInput, URL pmExpect) throws IOException {
+		ResourceSetImpl resourceSet = new ResourceSetImpl();
+		this.pmInput = (TypeGraph) loadModel(resourceSet, pmInput).get(0);
+		this.pmExpect = (TypeGraph) loadModel(resourceSet, pmExpect).get(0);
 		LOGGER.log(Level.INFO, "Perform test on project: " + name);
+	}
+
+	/**
+	 * Loads the model from the given URL into the reousce set
+	 * 
+	 * @param set The resource set
+	 * @param url The url to the model
+	 * @return The contents of the model
+	 * @throws IOException If reading the model failed
+	 */
+	private EList<EObject> loadModel(ResourceSetImpl set, URL url) throws IOException {
+		Resource resource = set.createResource(URI.createFileURI(url.getFile()));
+		resource.load(url.openStream(), Collections.emptyMap());
+		return resource.getContents();
 	}
 
 	/**
@@ -95,23 +98,14 @@ public class AccessAnalysisTest {
 	 */
 	@Parameters(name = "{index}: Test HulkAPI on \"{0}\"")
 	public static Collection<Object[]> collectProjects() throws CoreException, GitCloneException, IOException {
-		File location = new File(ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile(), "repository");
-
-		new GitTools("https://github.com/GRaViTY-Tool/gravity-evaluation-data.git", location, true, false).close();
-		Map<IProject, URL> importProjects = EclipseProjectUtil
-				.importProjects(new File(location, "gravity-evaluation-data")).parallelStream().map(p -> {
-					URL entry = Platform.getBundle("org.gravity.hulk.tests").getEntry("expect/" + p.getName() + ".xmi");
-					if (entry == null) {
-						return null;
-					} else {
-						return new AbstractMap.SimpleEntry<IProject, URL>(p, entry);
-					}
-				}).filter(Objects::nonNull)
-				.collect(Collectors.toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue));
-		Collection<Object[]> testData = TestHelper.prepareTestData(importProjects.keySet());
-		return testData.parallelStream()
-				.map(o -> new Object[] { o[0], o[1], importProjects.get(((IJavaProject) o[1]).getProject()) })
-				.collect(Collectors.toList());
+		Bundle bundle = Platform.getBundle("org.gravity.hulk.tests");
+		Collection<Object[]> results = new ArrayList<>(projects.length);
+		for(String name : projects) {
+			URL input = bundle.getEntry("input/"+name+".xmi");
+			URL expect = bundle.getEntry("expect/"+name+".xmi");
+			results.add(new Object[] {name, input, expect});
+		}
+		return results;
 	}
 
 	/**
@@ -124,21 +118,19 @@ public class AccessAnalysisTest {
 	 */
 	@Test
 	public void testAccessAnalysis() throws TransformationFailedException {
-		IProgressMonitor monitor = new NullProgressMonitor();
-		TypeGraph pg = GravityAPI.createProgramModel(javaProject, monitor);
-
+		String location = pmInput.eResource().getURI().toString();
 		HAntiPatternGraph apg = AntipatterngraphFactory.eINSTANCE.createHAntiPatternGraph();
-		apg.setPg(pg);
+		apg.setPg(pmInput);
 
 		HAntiPatternDetection hulk = HulkFactory.eINSTANCE.createHAntiPatternDetection();
 		hulk.setApg(apg);
-		hulk.setProgramlocation(javaProject.getProject().getLocation().toString());
+		hulk.setProgramlocation(location);
 
 		HIGAMCalculator igam = MetricsFactory.eINSTANCE.createHIGAMCalculator();
 		igam.setHAntiPatternHandling(hulk);
 		assertTrue(igam.detect(apg));
 		compareResults(igam.getHAnnotation());
-
+		
 		HIGATCalculator igat = MetricsFactory.eINSTANCE.createHIGATCalculator();
 		igat.setHAntiPatternHandling(hulk);
 		assertTrue(igat.detect(apg));
