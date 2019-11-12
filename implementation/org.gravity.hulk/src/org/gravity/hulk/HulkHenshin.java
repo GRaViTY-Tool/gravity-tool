565fe20421b5d1fee2b6c3856bc9e95aed9b4815
Sven Peldszus <speldszus@uni-koblenz.de>
2019-11-12 17:54:22 +0100

    Improved implementation

--- implementation/org.gravity.hulk/src/org/gravity/hulk/HulkHenshin.java
+++ implementation/org.gravity.hulk/src/org/gravity/hulk/HulkHenshin.java
@@ -1,8 +1,8 @@
 /**
  * <copyright>
- * Copyright (c) 2010-2014 Henshin developers. All rights reserved. 
- * This program and the accompanying materials are made available 
- * under the terms of the Eclipse Public License v1.0 which 
+ * Copyright (c) 2010-2014 Henshin developers. All rights reserved.
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0 which
  * accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * </copyright>
@@ -47,6 +47,7 @@ import org.gravity.eclipse.io.ExtensionFileVisitor;
 import org.gravity.hulk.antipatterngraph.AntipatterngraphPackage;
 import org.gravity.hulk.antipatterngraph.antipattern.AntipatternPackage;
 import org.gravity.hulk.antipatterngraph.values.HRelativeValueConstants;
+import org.gravity.security.annotations.AnnotationsPackage;
 import org.gravity.typegraph.basic.BasicPackage;
 
 public class HulkHenshin {
@@ -61,17 +62,18 @@ public class HulkHenshin {
 	private HashSet<Rule> executed;
 
 	public static void main(String[] args) throws IOException {
-		HenshinResourceSet resourceSet = new HenshinResourceSet();
+		final HenshinResourceSet resourceSet = new HenshinResourceSet();
+		resourceSet.getPackageRegistry().put(AnnotationsPackage.eNS_URI, AnnotationsPackage.eINSTANCE);
 		resourceSet.getPackageRegistry().put(BasicPackage.eNS_URI, BasicPackage.eINSTANCE);
 		resourceSet.getPackageRegistry().put(AntipatternPackage.eNS_URI, AntipatternPackage.eINSTANCE);
 
-		String model = "instances/iTrust21.0.xmi";
-		Resource resource = resourceSet.getResource(URI.createURI(model), true);
+		final String model = "instances/pm.xmi";
+		final Resource resource = resourceSet.getResource(URI.createURI(model), true);
 
-		Engine engine = new EngineImpl();
-		EGraphImpl graph = new EGraphImpl(resource);
+		final Engine engine = new EngineImpl();
+		final EGraphImpl graph = new EGraphImpl(resource);
 
-		HulkHenshin hulk = new HulkHenshin();
+		final HulkHenshin hulk = new HulkHenshin();
 		hulk.loadRules(resourceSet, new File("rules"));
 		hulk.execute(engine, graph, hulk.getRule(AntipatternPackage.eINSTANCE.getHBlobAntiPattern()));
 
@@ -81,14 +83,14 @@ public class HulkHenshin {
 	}
 
 	private Rule getRule(EClass type) {
-		return creates.get(type);
+		return this.creates.get(type);
 	}
 
 	private void executeAllRules(HenshinResourceSet set, Engine engine, EGraphImpl graph, File folder)
 			throws IOException {
-		Set<Rule> rules = loadRules(set, folder);
-		for (Rule rule : rules) {
-			if (!executed.contains(rule)) {
+		final Set<Rule> rules = loadRules(set, folder);
+		for (final Rule rule : rules) {
+			if (!this.executed.contains(rule)) {
 				execute(engine, graph, rule);
 			}
 		}
@@ -97,26 +99,26 @@ public class HulkHenshin {
 
 	/**
 	 * Loads the rules from the folder into the resource set
-	 * 
+	 *
 	 * @param set    The resource set
 	 * @param folder The folder containing rules
 	 * @return The loaded rules
 	 * @throws IOException If an I/O error occurred while searching the folder
 	 */
 	private Set<Rule> loadRules(HenshinResourceSet set, File folder) throws IOException {
-		Set<Rule> rules = new HashSet<>();
-		ExtensionFileVisitor visitor = new ExtensionFileVisitor(".henshin");
+		final Set<Rule> rules = new HashSet<>();
+		final ExtensionFileVisitor visitor = new ExtensionFileVisitor(".henshin");
 		Files.walkFileTree(folder.toPath(), visitor);
-		for (Path ruleFile : visitor.getFiles()) {
-			Module module = set.getModule(ruleFile.toAbsolutePath().toString(), false);
+		for (final Path ruleFile : visitor.getFiles()) {
+			final Module module = set.getModule(ruleFile.toAbsolutePath().toString(), false);
 			rules.addAll(module.getAllRules());
 		}
 
-		creates = new HashMap<>();
-		executed = new HashSet<>();
-		for (Rule rule : rules) {
-			for (EClass create : getCreates(rule)) {
-				if (creates.put(create, rule) != null) {
+		this.creates = new HashMap<>();
+		this.executed = new HashSet<>();
+		for (final Rule rule : rules) {
+			for (final EClass create : getCreates(rule)) {
+				if (this.creates.put(create, rule) != null) {
 					throw new IllegalStateException(
 							"The annotation \"" + create.getName() + "\" is created by multiple rules");
 				}
@@ -126,57 +128,57 @@ public class HulkHenshin {
 	}
 
 	private void execute(Engine engine, EGraphImpl graph, Rule rule) {
-		Set<EClass> requires = getRequires(rule);
+		final Set<EClass> requires = getRequires(rule);
 		if (!requires.isEmpty()) {
 			System.out.println("Execure requirements of rule: " + rule.getName());
 			System.out.println(requires.parallelStream().map(type -> type.getName()).collect(Collectors.joining(", ")));
 		}
-		for (EClass requirement : requires) {
-			Rule requiredRule = creates.get(requirement);
+		for (final EClass requirement : requires) {
+			final Rule requiredRule = this.creates.get(requirement);
 			if (requiredRule == null) {
 				throw new IllegalStateException(
 						"No rule for creating the annotation \"" + requirement.getName() + "\" has been loaded");
 			}
-			if (!executed.contains(requiredRule)) {
+			if (!this.executed.contains(requiredRule)) {
 				execute(engine, graph, requiredRule);
 			}
 		}
 
 		LOGGER.info("Execute rule: " + rule.getName());
-		Parameter threshold = rule.getParameter(THRESHOLD);
+		final Parameter threshold = rule.getParameter(THRESHOLD);
 		double thresholdValue = -1;
 		if (threshold != null) {
 			thresholdValue = getThresholdValue(threshold, engine, rule, graph);
 		}
-		Iterable<Match> matches = engine.findMatches(rule, graph, null);
+		final Iterable<Match> matches = engine.findMatches(rule, graph, null);
 		int i = 0;
-		for (Match match : matches) {
+		for (final Match match : matches) {
 			if (threshold != null) {
 				match.setParameterValue(threshold, thresholdValue);
 			}
 			if (!engine.findMatches(rule, graph, match).iterator().hasNext()) {
 				continue;
 			}
-			boolean success = new RuleApplicationImpl(engine, graph, rule, match).execute(null);
+			final boolean success = new RuleApplicationImpl(engine, graph, rule, match).execute(null);
 			if (!success) {
 				LOGGER.error("Error: " + match);
 			} else {
 				i++;
 			}
 		}
-		executed.add(rule);
+		this.executed.add(rule);
 		LOGGER.info("Applications=" + i);
 	}
 
 	protected static Set<EClass> getCreates(Rule rule) {
-		Set<EClass> creates = new HashSet<>();
-		TreeIterator<EObject> eAllContents = rule.eAllContents();
+		final Set<EClass> creates = new HashSet<>();
+		final TreeIterator<EObject> eAllContents = rule.eAllContents();
 		while (eAllContents.hasNext()) {
-			EObject eObj = eAllContents.next();
+			final EObject eObj = eAllContents.next();
 			if (eObj instanceof Node) {
-				Node node = (Node) eObj;
+				final Node node = (Node) eObj;
 				if (node.getAction() != null && node.getAction().getType() == Type.CREATE) {
-					EClass type = node.getType();
+					final EClass type = node.getType();
 					if (AntipatterngraphPackage.eINSTANCE.getHAnnotation().isSuperTypeOf(type)) {
 						creates.add(type);
 					}
@@ -188,14 +190,14 @@ public class HulkHenshin {
 	}
 
 	protected static Set<EClass> getRequires(Rule rule) {
-		Set<EClass> requires = new HashSet<>();
-		TreeIterator<EObject> eAllContents = rule.eAllContents();
+		final Set<EClass> requires = new HashSet<>();
+		final TreeIterator<EObject> eAllContents = rule.eAllContents();
 		while (eAllContents.hasNext()) {
-			EObject eObj = eAllContents.next();
+			final EObject eObj = eAllContents.next();
 			if (eObj instanceof Node) {
-				Node node = (Node) eObj;
+				final Node node = (Node) eObj;
 				if (node.getAction() != null && node.getAction().getType() != Type.CREATE) {
-					EClass type = node.getType();
+					final EClass type = node.getType();
 					if (AntipatterngraphPackage.eINSTANCE.getHAnnotation().isSuperTypeOf(type)) {
 						requires.add(type);
 					}
@@ -207,33 +209,33 @@ public class HulkHenshin {
 	}
 
 	private static double getThresholdValue(Parameter threshold, Engine engine, Rule rule, EGraphImpl graph) {
-		HRelativeValueConstants constant = HRelativeValueConstants.getByName(threshold.getDescription());
+		final HRelativeValueConstants constant = HRelativeValueConstants.getByName(threshold.getDescription());
 		if (constant == null) {
 			throw new IllegalStateException("Misspelled relative constant in rule \"" + rule.getName() + "\"");
 		}
-		String value = getThresholdCondition(rule);
+		final String value = getThresholdCondition(rule);
 
-		SortedSet<Number> numbers = new TreeSet<>();
-		Iterable<Match> matches = engine.findMatches(rule, graph, null);
-		for (Match match : matches) {
-			ScriptEngine script = engine.getScriptEngine();
-			for (Parameter param : rule.getParameters()) {
-				Object parameterValue = match.getParameterValue(param);
+		final SortedSet<Number> numbers = new TreeSet<>();
+		final Iterable<Match> matches = engine.findMatches(rule, graph, null);
+		for (final Match match : matches) {
+			final ScriptEngine script = engine.getScriptEngine();
+			for (final Parameter param : rule.getParameters()) {
+				final Object parameterValue = match.getParameterValue(param);
 				if (parameterValue != null) {
 					script.put(param.getName(), parameterValue);
 				}
 			}
 			try {
-				Object res = script.eval(value);
+				final Object res = script.eval(value);
 				if (res instanceof Number) {
 					numbers.add((Number) res);
 				}
-			} catch (ScriptException e) {
+			} catch (final ScriptException e) {
 				LOGGER.error(e.getMessage(), e);
 			}
 		}
 
-		int size = numbers.size();
+		final int size = numbers.size();
 		switch (constant) {
 		case VERY_LOW:
 			return (double) numbers.toArray()[size * 1 / 6];
@@ -251,13 +253,13 @@ public class HulkHenshin {
 
 	/**
 	 * Get the condition of the threshold node
-	 * 
+	 *
 	 * @param rule The rule
 	 * @return The condition
 	 */
 	private static String getThresholdCondition(Rule rule) {
-		for (AttributeCondition cond : rule.getAttributeConditions()) {
-			String conditionText = cond.getConditionText();
+		for (final AttributeCondition cond : rule.getAttributeConditions()) {
+			final String conditionText = cond.getConditionText();
 			if (conditionText.contains(THRESHOLD)) {
 				return conditionText.replaceAll(THRESHOLD + "\\s*(>|>=|=|<=|<)", "")
 						.replaceAll("(>|>=|=|<=|<)\\s*" + THRESHOLD, "");
