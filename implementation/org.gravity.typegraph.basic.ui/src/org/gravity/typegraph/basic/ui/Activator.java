b14841fc0a6d1c37ab56abc1b4787a4748f25bfe
Sven Peldszus <speldszus@uni-koblenz.de>
2021-01-15 15:34:41 +0100

    Added missing project files

--- implementation/org.gravity.typegraph.basic.ui/src/org/gravity/typegraph/basic/ui/Activator.java
+++ implementation/org.gravity.typegraph.basic.ui/src/org/gravity/typegraph/basic/ui/Activator.java
@@ -22,12 +22,12 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.
 	 * BundleContext)
 	 */
 	@Override
-	public void start(BundleContext context) throws Exception {
+	public void start(final BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 		viewpoints = new HashSet<>();
@@ -37,12 +37,12 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see
 	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	@Override
-	public void stop(BundleContext context) throws Exception {
+	public void stop(final BundleContext context) throws Exception {
 		plugin = null;
 		if (viewpoints != null) {
 			for (final Viewpoint viewpoint : viewpoints) {
@@ -56,7 +56,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 * 
+	 *
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
