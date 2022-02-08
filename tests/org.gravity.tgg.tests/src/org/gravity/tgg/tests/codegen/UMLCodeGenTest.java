905c5ba8ea9da8edc5632d19a1a372808443d2ae
Sven Peldszus <sven.peldszus@rub.de>
2022-02-08 09:24:18 +0100

    Added more assertions and refresh project

--- tests/org.gravity.tgg.tests/src/org/gravity/tgg/tests/codegen/UMLCodeGenTest.java
+++ tests/org.gravity.tgg.tests/src/org/gravity/tgg/tests/codegen/UMLCodeGenTest.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 
 import org.apache.log4j.BasicConfigurator;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
@@ -31,12 +32,16 @@ import org.junit.Test;
  */
 public class UMLCodeGenTest {
 
+	private static final String PROJECT_NAME = "CodeGenTest";
+	private static final String PACKAGE_NAME = "namespace";
+	private static final String TYPE_NAME = "Type";
+
 	@Test
 	public void testSimpleCodeGen()
 			throws CoreException, IOException, TransformationFailedException, DuplicateProjectNameException {
 		BasicConfigurator.configure();
 		final var monitor = new NullProgressMonitor();
-		final var project = EclipseProjectUtil.createProject("CodeGenTest", true, monitor);
+		final var project = EclipseProjectUtil.createProject(PROJECT_NAME, true, monitor);
 		try {
 			final var umlFile = Transformation.getUMLFile(project, monitor);
 			generateUMLModel(umlFile);
@@ -44,9 +49,14 @@ public class UMLCodeGenTest {
 			final var status = Transformation.generateCode(project, umlFile, monitor);
 			assertEquals(Status.OK_STATUS, status);
 
+			project.refreshLocal(IResource.DEPTH_INFINITE, monitor);
+
+			final var srcFile= project.getFile("src/"+PACKAGE_NAME+'/'+TYPE_NAME+".java");
+			assertTrue(srcFile.exists());
+
 			final var java = JavaProjectUtil.getJavaProject(project);
 			final var types = JavaASTUtil.getTypesForProject(java);
-			assertTrue(types.containsKey("namespace.Type"));
+			assertTrue(types.containsKey(PACKAGE_NAME+'.'+TYPE_NAME));
 		} finally {
 			project.delete(true, monitor);
 		}
@@ -62,8 +72,8 @@ public class UMLCodeGenTest {
 	private Model generateUMLModel(final IFile umlFile) throws IOException {
 		final var resource = new ResourceSetImpl().createResource(EMFUtil.getPlatformResourceURI(umlFile));
 		final var root = UMLFactory.eINSTANCE.createModel();
-		root.setName("UMLSingleClass");
-		root.createNestedPackage("namespace").createOwnedClass("Type", false);
+		root.setName(PROJECT_NAME);
+		root.createNestedPackage(PACKAGE_NAME).createOwnedClass(TYPE_NAME, false);
 		resource.getContents().add(root);
 		resource.save(Collections.emptyMap());
 		return root;
