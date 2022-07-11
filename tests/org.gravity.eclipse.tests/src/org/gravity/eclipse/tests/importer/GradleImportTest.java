7abbdc1a70a15defb193b616574384beaaabb4d0
Sven Peldszus <sven.peldszus@rub.de>
2022-07-11 13:50:41 +0200

    Improved TGG and Headless

--- tests/org.gravity.eclipse.tests/src/org/gravity/eclipse/tests/importer/GradleImportTest.java
+++ tests/org.gravity.eclipse.tests/src/org/gravity/eclipse/tests/importer/GradleImportTest.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
 
-import org.eclipse.core.runtime.CoreException;
+import org.apache.log4j.Logger;
 import org.gravity.eclipse.importer.ImportException;
 import org.gravity.eclipse.importer.gradle.GradleImport;
 import org.junit.runner.RunWith;
@@ -15,12 +15,15 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class GradleImportTest extends ImportTest {
 
+	private static final Logger LOGGER = Logger.getLogger(GradleImportTest.class);
+
 	public GradleImportTest(final String name, final File projectLocation, final Map<String, String> expected) throws IOException, ImportException {
 		super(new GradleImport(projectLocation, true), expected);
+		LOGGER.info("Run: "+name);
 	}
 
 	@Parameters(name = "{index}: Import gradle project: {0}")
-	public static Collection<Object[]> getTestProjects() throws CoreException, IOException {
+	public static Collection<Object[]> getTestProjects() throws IOException {
 		return getTestProjects(".gradle.csv");
 	}
 }
