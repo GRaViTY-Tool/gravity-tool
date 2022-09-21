811859b1005e9220e9fbec21530773295d681fb4
Sven Peldszus <sven.peldszus@rub.de>
2022-09-21 18:47:26 +0200

    Added test project for pom dependencies

---
+++ tests/org.gravity.eclipse.tests/data/import/maven-pom-dependency/module1/src/main/java/org/gravity/ParentClass.java
@@ -0,0 +1,12 @@
+package org.gravity;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentClass {
+
+	public static void main(final String[] args) {
+	}
+
+}
