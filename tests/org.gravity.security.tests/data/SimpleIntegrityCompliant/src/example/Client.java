31cde300a08ffcd5de6506e42b0045dc42829a82
Sven Peldszus <sven.peldszus@rub.de>
2023-10-26 09:12:48 +0200

    Enabled ui for test

--- tests/org.gravity.security.tests/data/SimpleIntegrityCompliant/src/example/Client.java
+++ tests/org.gravity.security.tests/data/SimpleIntegrityCompliant/src/example/Client.java
@@ -1,6 +1,7 @@
 package example;
 
 import org.gravity.security.annotations.requirements.Critical;
+import org.gravity.security.annotations.requirements.Integrity;
 
 @Critical(integrity = "method(boolean):boolean")
 public class Client {
